76b9732c50bda8bab90793d0eb70bd6ad27da6de
paras3103 <81362225+paras3103@users.noreply.github.com>
2026-01-23 11:35:24 +0000

    datatype

---
+++ 01-basics/datatype.js
@@ -0,0 +1,24 @@
+let userNumber = "124";
+let userEmail = "ashvini@gmail.com";    
+let userPassword = "ashvini@123";
+let userCity = "Chaprajpur";    
+let isLoggedIn = true;
+let userAge = 24;
+let userBalance = 2500.50;
+
+console.log(typeof userBalance);
+console.table([userNumber, userEmail, userPassword, userCity, isLoggedIn, userAge, userBalance]);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
